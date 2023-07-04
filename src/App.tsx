98e1514cfd2eb0a261a9775960627da19cdf8707
linhaishe <42030043+linhaishe@users.noreply.github.com>
2023-07-04 22:55:28 +0800

    feat: 整合多个状态声明为一个

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,39 @@
 import React, { useRef, type ChangeEvent, useState } from 'react';
-import './index.scss';
-import { type RegionStats } from './components/types';
 import Table from './components/table';
 import { calculateValuesByParams } from './utils/calculateValuesByParams';
 import { calculateWeightStats } from './utils/calculateWeightStats';
 
 const App = (): React.JSX.Element => {
   const fileInputRef = useRef<HTMLInputElement>(null);
-  const [statsByRegion, setStatsByRegion] = useState<Record<string, RegionStats>>({});
-  const [statsByresource, setStatsByresource] = useState<Record<string, RegionStats>>({});
-  const [statsRegionByYear, setStatsRegionByYear] = useState<Record<string, RegionStats>>({});
-  const [weightStats, setweightStats] = useState<{
-    max: number;
-    min: number;
-    median: number;
-  }>({
-    max: 0,
-    min: 0,
-    median: 0,
+  const [stats, setStats] = useState({
+    statsByRegion: {},
+    statsByResource: {},
+    statsRegionByYear: {},
+    weightStats: {
+      max: 0,
+      min: 0,
+      median: 0,
+    },
   });
 
   const handleButtonClick = (): void => {
     fileInputRef.current?.click();
+  };
+
+  const handleFileLoad = (event: ProgressEvent<FileReader>): void => {
+    const fileContent = event.target?.result;
+    const data = JSON.parse(fileContent as string);
+    const regionData = calculateValuesByParams(data.nodes, ['region']);
+    const resourceData = calculateValuesByParams(data.nodes, ['resource']);
+    const regionByYearData = calculateValuesByParams(data.nodes, ['region', 'year']);
+    const weightData = calculateWeightStats(data.nodes, 'weight');
+
+    setStats({
+      statsByRegion: regionData,
+      statsByResource: resourceData,
+      statsRegionByYear: regionByYearData,
+      weightStats: weightData,
+    });
   };
 
   const handleFileSelect = (event: ChangeEvent<HTMLInputElement>): void => {
@@ -41,33 +53,16 @@ const App = (): React.JSX.Element => {
     reader.readAsText(file);
   };
 
-  const handleFileLoad = (event: ProgressEvent<FileReader>): void => {
-    const fileContent = event.target?.result;
-    const data = JSON.parse(fileContent as string);
-
-    const regionData = calculateValuesByParams(data.nodes, ['region']);
-    setStatsByRegion(regionData);
-
-    const resourceData = calculateValuesByParams(data.nodes, ['resource']);
-    setStatsByresource(resourceData);
-
-    const regionByYearData = calculateValuesByParams(data.nodes, ['region', 'year']);
-    setStatsRegionByYear(regionByYearData);
-
-    const wightData = calculateWeightStats(data.nodes, 'weight');
-    setweightStats(wightData);
-  };
-
   return (
     <div className="data-container">
       <button onClick={handleButtonClick}>上传文件</button>
       <input type="file" ref={fileInputRef} onChange={handleFileSelect} style={{ display: 'none' }} />
-      <Table data={statsByRegion} title={'region下value相关值'} sortParams={'Region'} />
-      <Table data={statsByresource} title={'resource下value相关值'} sortParams={'Resource'} />
-      <Table data={statsRegionByYear} title={'region下每年value相关值'} sortParams={'Region by year'} />
+      <Table data={stats.statsByRegion} title={'region下value相关值'} sortParams={'Region'} />
+      <Table data={stats.statsByResource} title={'resource下value相关值'} sortParams={'Resource'} />
+      <Table data={stats.statsRegionByYear} title={'region下每年value相关值'} sortParams={'Region by year'} />
       <div>
         <h5 style={{ marginTop: '20px' }}>数据集中，weight 最大值、最小值、中位值</h5>
-        {[weightStats].map((value, index) => {
+        {[stats.weightStats].map((value, index) => {
           return (
             <ul key={index}>
               <li>max: {value.max}</li>
